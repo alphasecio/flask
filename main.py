2447f87f3ea301a57577af2eb8465caeff1607eb
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-02-25 00:54:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -8,4 +7,4 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(port=5000)
