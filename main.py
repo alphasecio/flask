bba05ab762ddec0bb8510ed55339b74fc46e73a9
mohamed samy <69082397+MohamedSamy853@users.noreply.github.com>
2023-05-23 11:49:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+import pickle
+from flask import Flask , request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+with open("model_pipr.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/")
+def home():
+    return "hellpo from home page"
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    age = request.args.get("age")
+    gender = request.args.get("gender")
+    spo2 = request.args.get("spo2")
+    pr = request.args.get("pr")
+    nCov2 = request.args.get("nCoV2")
+    data = [[float(age), float(gender), float(spo2), float(pr), float(nCov2)]]
+    res = model.predict(data)[0]
+    return jsonify({"oxy_flow":str(abs(res))})
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000)
